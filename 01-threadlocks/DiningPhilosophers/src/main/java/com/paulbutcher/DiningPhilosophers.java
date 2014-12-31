7eed1e0a3aaa03f6c4b1ce9dfe5df36224da3eb3
gothug <kojuhovskiy@gmail.com>
2014-12-31 17:14:24 +0400

    DiningPhilosophers experiments

--- 01-threadlocks/DiningPhilosophers/src/main/java/com/paulbutcher/DiningPhilosophers.java
+++ 01-threadlocks/DiningPhilosophers/src/main/java/com/paulbutcher/DiningPhilosophers.java
@@ -11,16 +11,18 @@ package com.paulbutcher;
 public class DiningPhilosophers {
 
   public static void main(String[] args) throws InterruptedException {
-    Philosopher[] philosophers = new Philosopher[5];
-    Chopstick[] chopsticks = new Chopstick[5];
+    int N = 5;
+
+    Philosopher[] philosophers = new Philosopher[N];
+    Chopstick[] chopsticks = new Chopstick[N];
     
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < N; ++i)
       chopsticks[i] = new Chopstick(i);
-    for (int i = 0; i < 5; ++i) {
-      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
+    for (int i = 0; i < N; ++i) {
+      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % N]);
       philosophers[i].start();
     }
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < N; ++i)
       philosophers[i].join();
   }
 }
--- 01-threadlocks/DiningPhilosophers/src/main/java/com/paulbutcher/Philosopher.java
+++ 01-threadlocks/DiningPhilosophers/src/main/java/com/paulbutcher/Philosopher.java
@@ -26,10 +26,10 @@ class Philosopher extends Thread {
         ++thinkCount;
         if (thinkCount % 10 == 0)
           System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
+        Thread.sleep(random.nextInt(10));     // Think for a while
         synchronized(left) {                    // Grab left chopstick 
           synchronized(right) {                 // Grab right chopstick 
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
+            Thread.sleep(random.nextInt(100)); // Eat for a while
           }
         }
       }
